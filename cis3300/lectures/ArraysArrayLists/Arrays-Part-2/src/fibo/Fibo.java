a450e902dbe633067420bdc4e4c4cdd9cd567a64
Jason Powell <powellja@nsula.edu>
2026-08-24 14:32:49 -0500

    Arrays Part 2 code

---
+++ cis3300/lectures/ArraysArrayLists/Arrays-Part-2/src/fibo/Fibo.java
@@ -0,0 +1,37 @@
+package fibo;
+
+import java.util.Arrays;
+
+public class Fibo {
+
+	public static void main(String[] args) {
+			
+		int[] memo = new int[100];
+		
+		System.out.println(Arrays.toString(memo));
+		Arrays.fill(memo, -1);
+		
+		System.out.println(   fibo(20, memo)   );
+		
+		System.out.println(Arrays.toString(memo));
+	}
+
+	public static int fibo(int n, int[] memo) {
+		if (  memo[n] != -1  ) {
+		 return memo[n];
+		}
+		else {
+			if (n <= 1) {
+				memo[n] = n;
+				System.out.println(Arrays.toString(memo));
+				return memo[n];
+			}
+			memo[n] = fibo(n-1, memo) + fibo(n-2, memo);
+			System.out.println(Arrays.toString(memo));
+			return memo[n];
+		}
+		
+		
+	}
+
+}
