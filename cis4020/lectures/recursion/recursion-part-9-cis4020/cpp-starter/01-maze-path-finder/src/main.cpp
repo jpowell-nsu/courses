5c7ab844e8e4a03da4d9d36cf22ed9d66b13b3f9
Jason Powell <powellja@nsula.edu>
2026-08-28 12:14:11 -0500

    cis4020 recursion part 9 started

--- cis4020/lectures/recursion/recursion-part-9-cis4020/cpp-starter/01-maze-path-finder/src/main.cpp
+++ cis4020/lectures/recursion/recursion-part-9-cis4020/cpp-starter/01-maze-path-finder/src/main.cpp
@@ -9,9 +9,32 @@ int main() {
         {'.', '#', '#', '#', '.'},
         {'.', '.', '.', '.', 'E'}
     };
+
     bool visited[5][COLS] = {};
 
     std::cout << std::boolalpha << hasPath(maze, visited, 5, 0, 0, 4, 4) << std::endl;
+ void dfs (char maze, int posy, int posx,  )
+ {
+    if(i <cols or if i> Cols)
+    {
+    visited[i] = true; 
+    }
 
+    if(i + 1 == '.' )
+    {
+    visited[i][j]= true;
+    dfs(maze, i+1, posy)
+    }
+
+    if(i - 1 ='.' )
+    {
+    visited[i][j]= true;
+    dfs(maze, +1, posy)
+    }
+    
+    
+
+
+ }
     return 0;
 }
